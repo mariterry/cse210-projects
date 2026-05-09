0fb7285eb86931ede416e43efc8b6d420573dfe9
mariterry <marriterymakdossantos@gmail.com>
2026-05-09 22:40:59 +0200

    Finished prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("I love you");
+
+        Console.Write("What is your favorite color: ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");
     }
 }
