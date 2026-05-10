b6a2c94bcd847fc5f74b4bda957b030c97cf8ba5
mariterry <marriterymakdossantos@gmail.com>
2026-05-10 19:08:18 +0200

    guess game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+        while (playAgain == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+
+            int guess = 0;
+            int guessCount = 0;
+
+            while (guess != number)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+               
+                guessCount++;         
+                
+                if (guess == number)
+                {
+                    Console.WriteLine("You guess it");
+                }
+
+                else if (guess < number)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+
+                else 
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+            }    
+            Console.WriteLine($"You made {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine().ToLower();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
