08287ec911b8ace22ebcb4c8dcce650403530355
mariterry <marriterymakdossantos@gmail.com>
2026-06-09 21:58:07 +0200

    mind game

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Start breathing activity");
+            Console.WriteLine("  2. Start reflection activity");
+            Console.WriteLine("  3. Start listing activity");
+            Console.WriteLine("  4. Quit");
+            Console.Write("Select a choice from the menu: ");
+
+            choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    BreathingActivity breathing = new BreathingActivity();
+                    breathing.Run();
+                    break;
+
+                case "2":
+                    ReflectionActivity reflection = new ReflectionActivity();
+                    reflection.Run();
+                    break;
+
+                case "3":
+                    ListingActivity listing = new ListingActivity();
+                    listing.Run();
+                    break;
+
+                case "4":
+                    Console.WriteLine("Goodbye!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+
+            if (choice != "4")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Press Enter to return to the menu.");
+                Console.ReadLine();
+            }
+        }
     }
 }
