ef234f1532f88b6afb4fda29cb8d667b58aa414c
mariterry <marriterymakdossantos@gmail.com>
2026-06-15 20:18:57 +0200

    new shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 4);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Purple", 5, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Green", 7);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
